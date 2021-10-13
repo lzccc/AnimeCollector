b72bc629e713ab96967ef3d9bd806de391c8d990
lzccc <961191365@qq.com>
2021-10-12 21:03:29 -0700

    t

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ZIC
+          Learn React ZIc
         </a>
         <AmplifySignOut />
       </header>
